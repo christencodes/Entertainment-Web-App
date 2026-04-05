d794d3d027662166f89db2d1275bcc5bb12e10d3
christenally <13616443+Christenally@users.noreply.github.com>
2026-04-05 02:41:47 -0500

    SVGs are hard

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,42 @@
-import Logo from "/public/assets/logo.svg?react";
-import HomeIcon from "/public/assets/icon-nav-home.svg?react";
+import Logo from "/public/assets/logo.svg";
 
+import { HomeIcon } from "./Icons";
+import { MovieIcon } from "./Icons";
+import { TVIcon } from "./Icons";
+import BookmarkIcon from "/public/assets/icon-nav-bookmark.svg";
+// fill="#5A698F" <----------- inactive color
 export default function Navbar() {
   return (
-    <div className="w-full min-h-14 bg-blue900">
-      <section className="logo">
-        <Logo fill="#ffffff"></Logo>
-      </section>
-      <section className="navigation">
+    <div className="flex items-center justify-between w-full min-h-14 bg-blue900">
+      <section className="logo"></section>
+      <section className="navigation flex items-center justify-center gap-6">
         <div>
-          <HomeIcon fill="white"></HomeIcon>
+          <HomeIcon
+            width={20}
+            height={20}
+            viewBox={"0 0 20 20"}
+            fill={"white"}
+          ></HomeIcon>
         </div>
-        <div></div>
-        <div></div>
-        <div></div>
+        <div>
+          <MovieIcon
+            width={20}
+            height={20}
+            viewBox={"0 0 20 20"}
+            fill={"white"}
+          ></MovieIcon>
+        </div>
+        <div>
+          <TVIcon
+            width={20}
+            height={20}
+            viewBox={"0 0 20 20"}
+            fill={"white"}
+          ></TVIcon>
+        </div>
+        <div>
+          <img src={BookmarkIcon} className="w-4 h-4" alt="" />
+        </div>
       </section>
       <section className="avatar"></section>
     </div>
